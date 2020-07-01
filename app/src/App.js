45d1cb4fb3b8a45770a0bb33a4c9e86f965a0d36
Thong <tho2@gmail.com>
2020-07-01 14:41:49 -0400

    Created button and textfield

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      iteration: 0
+    }
+
+    this.handleIteration = this.handleIteration.bind(this);
+
+  }
+
+  handleIteration(e){
+    var iter = parseInt(e.target.value, 10);
+    if(!isNaN(iter)){
+      this.setState({iteration: iter});
+    }
+    else{
+      if(e.target.value == ""){
+        this.setState({iteration: 0});
+      }
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="head">
+          <div className="set1">
+            <button className="button" style={{backgroundColor:'#2cbebe'}}>+</button>
+            <button className="button" style={{backgroundColor:'#2cbebe'}}>→</button>
+            <button className="button" style={{backgroundColor:'red'}}>x</button>
+          </div>
+          <div className="set2">
+            <label className="label">Iterations: </label>
+            <input type="text" id="iteration" className="textbox" value={this.state.iteration} onChange={this.handleIteration}></input>
+            <button className="button" style={{backgroundColor:'#4CAF50'}}>Start</button>
+            <button className="button" style={{backgroundColor:'red'}}>Stop</button>
+            <button className="button" style={{backgroundColor:'yellow', color:"black"}}>Data</button>
+            <button className="button" style={{backgroundColor:'yellow', color:"black"}}>Clear</button>
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
+
 }
 
 export default App;
