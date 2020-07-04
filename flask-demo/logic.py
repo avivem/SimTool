29219b8e745a9e4a19950e4b2dffe11a10bcb314
Aviv Elazar-Mittelman <19512757+avivem@users.noreply.github.com>
2020-07-04 01:35:53 -0400

    Entities now flow through a linked list system and print simple messages.

--- flask-demo/logic.py
+++ flask-demo/logic.py
@@ -4,8 +4,9 @@ import simpy
 
 #Basic building block of a system. It knows where an entity should go next.
 class Node(object):
-    def __init__(self, env):
+    def __init__(self, env, name):
         self.env = env
+        self.name = name
     def set_directed_to(self,obj):
         self.directed_to = obj
 
@@ -17,10 +18,9 @@ line, etc.
 
 """
 class BasicFlowEntity(object):
-    def __init__(self, env:simpy.Environment,name:str, nextLoc:Node, currentLoc:Node):
+    def __init__(self, env:simpy.Environment,name:str, currentLoc:Node):
         self.env = env
         self.name = name
-        self.nextLoc = nextLoc
         self.currentLoc = currentLoc
         
     def __str__(self):
@@ -32,23 +32,29 @@ class BasicFlowEntity(object):
             with self.currentLoc.resource.request() as req:
                 yield req
                 yield self.currentLoc.get_timer(self)
+                self.currentLoc = self.currentLoc.directed_to
+
         self.currentLoc.entities.append(self)
+        print(f'[{self.env.now}]:: {self} has reached endpoint {self.currentLoc}')
 
 #Node that generates flowing entities.
 class StartingPoint(Node):
-    def __init__(self,env,gen_func):
-        super().__init__(env)
+    def __init__(self,env,name,gen_func):
+        super().__init__(env,name)
         self.gen_func = gen_func
         self.directed_to = None
         self.entities = []
         self.count = 0
         self.action = env.process(self.run())
+    def __str__(self):
+        return self.name
     
     def run(self):
         while True:
             yield self.env.timeout(self.gen_func)
-            cur = BasicFlowEntity(self.env,f'Flow Entity {self.count}',self.directed_to.directed_to, self.directed_to)
-            self.env.process(cur.run())
+            entity = BasicFlowEntity(self.env,f'Flow Entity {self.count}',self.directed_to)
+            self.env.process(entity.run())
+            print(f'[{self.env.now}]:: {entity} has left {self}')
             self.count += 1
             
             
@@ -56,14 +62,14 @@ class StartingPoint(Node):
 #a dishwasher in a kitchen.
 class BasicComponent(Node):
     def __init__(self,env, name, capacity, time):
-        super().__init__(env)
+        super().__init__(env,name)
         self.name = name
         self.capacity = capacity
         self.resource = simpy.Resource(env,capacity)
         self.time = time
 
     def __str__(self):
-        return f'{self.name}'
+        return self.name
         
     #Returns a timeout event which represents the amount of time a component
     #needs to do it's thing.
@@ -73,15 +79,17 @@ class BasicComponent(Node):
 
 #Collection point for entities that have travelled through the system.
 class EndingPoint(Node):
-    def __init__(self,env):
-        super().__init__(env)
+    def __init__(self,env,name):
+        super().__init__(env,name)
         self.entities = []
+    def __str__(self):
+        return self.name
 
 env = simpy.Environment()
-st = StartingPoint(env, 2)
+st = StartingPoint(env, "Starting Point 1",2)
 b1 = BasicComponent(env,"Basic Component #1", 3, 7)
-ed = EndingPoint(env)
+ed = EndingPoint(env,"Ending Point 1")
 st.set_directed_to(b1)
 b1.set_directed_to(ed)
 env.process(st.run())
-env.run(until=100)
+env.run(until=300)
