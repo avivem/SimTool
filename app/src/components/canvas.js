63b9f5c4f8ecad15ec637469f6eaddc28dcfe697
Thong <tho2@gmail.com>
2020-07-04 22:48:08 -0400

    Remove testing with konva

--- app/src/components/canvas.js
+++ app/src/components/canvas.js
@@ -27,47 +27,7 @@ class Canvas extends Component{
         var layer = new Konva.Layer();
         stage.add(layer);
 
-        function updateObjects() {
-            nodeList.forEach((target) => {
-                var node = layer.findOne('#' + target.id);
-                node.x(target.x);
-                node.y(target.y);
-            });
-    //        this.props.stationNode.forEach((target) => {
-    //            var node = layer.findOne('#' + target.id);
-    //            node.x(target.x);
-    //            node.y(target.y);
-    //        });
-    //        this.props.endNode.forEach((target) => {
-    //            var node = layer.findOne('#' + target.id);
-    //            node.x(target.x);
-    //            node.y(target.y);
-     //         });
-            
-            layer.batchDraw();
-          }
-
-          nodeList.forEach((target) => {
-            var node = new Konva.Circle({
-              id: target.id,
-              fill: Konva.Util.getRandomColor(),
-              radius: 20 + Math.random() * 20,
-              shadowBlur: 10,
-              draggable: true,
-            });
-            layer.add(node);
-    
-            node.on('dragmove', () => {
-              // mutate the state
-              target.x = node.x();
-              target.y = node.y();
-    
-              // update nodes from the new state
-              updateObjects();
-            });
-          });
-    
-        updateObjects();
+        
 
         
     }
