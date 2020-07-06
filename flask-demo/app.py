1c96ecf7bce410389de524f6ff6414be6d0baafb
Aviv Elazar-Mittelman <19512757+avivem@users.noreply.github.com>
2020-07-05 21:06:20 -0400

    Added run function to demo and made refreshing basic page add more nodes.

--- flask-demo/app.py
+++ flask-demo/app.py
@@ -8,9 +8,9 @@ import io
 
 ###If you are running locally and wish to see output in terminal, comment this out.
 if __name__ != "__main__":
-    old_stdout = sys.stdout
-    new_stdout = io.StringIO()
-    sys.stdout = new_stdout
+	old_stdout = sys.stdout
+	new_stdout = io.StringIO()
+	sys.stdout = new_stdout
 
 app = Flask(__name__)
 api = Api(app)
@@ -23,9 +23,9 @@ NODES = {
 }
 
 class DataStore():
-    st = None
-    b1 = None
-    ed = None
+	st = None
+	bc = []
+	ed = None
 
 data = DataStore()
 
@@ -34,39 +34,48 @@ env = simpy.Environment()
 
 # create starting node
 class Start(Resource):
-    # 'get' function, but when you run http://127.0.0.1:5000/api/create in the browser
-    # it will call StartingPoint and create the starting node
+	# 'get' function, but when you run http://127.0.0.1:5000/api/create in the browser
+	# it will call StartingPoint and create the starting node
 	def get(self):
 		
 		data.st = StartingPoint(env, "Starting Point 1",2, 100)
-		return NODES
+		return "Starting Point 1 added"
 
 # create basic component
 class Basic(Resource):
 	# 'get' function, but when you run http://127.0.0.1:5000/api/basiccomponent in the browser
-    # it will call StartingPoint and create the starting node
+	# it will call StartingPoint and create the starting node
 	def get(self):
-		data.b1 = BasicComponent(env,"Basic Component #1", 3, 7) 
-		return NODES
+		data.bc.append(BasicComponent(env,f"Basic Component #{len(data.bc) + 1}", 3, 7))
+		if len(data.bc) == 1:
+			data.st.set_directed_to(data.bc[0])
+		else:
+			data.bc[len(data.bc)-2].set_directed_to(data.bc[len(data.bc)-1])
+		return f"Basic Component #{len(data.bc)} added"
 
 
 # create ending component
 class End(Resource):
 	# 'get' function, but when you run http://127.0.0.1:5000/api/basiccomponent in the browser
-    # it will call StartingPoint and create the starting node
+	# it will call StartingPoint and create the starting node
 	def get(self):
 		data.ed = EndingPoint(env,"Ending Point 1")
-		data.st.set_directed_to(data.b1)
-		data.b1.set_directed_to(data.ed)
+		data.bc[len(data.bc)-1].set_directed_to(data.ed)
+		return f"Ending Point #{len(data.bc)} added"
+		
+
+class Run(Resource):
+	def get(self):
 		env.process(data.st.run())
 		env.run(until=300)
-		return jsonify(new_stdout.getvalue().splitlines())
+		return new_stdout.getvalue().split('\n')
 
 
 # resource, route
 api.add_resource(Start, "/api/start")
 api.add_resource(Basic, "/api/basic")
 api.add_resource(End, "/api/end")
+api.add_resource(Run, "/api/run")
 
 
 
