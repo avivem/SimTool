08e13fbb81a87f6771fec212838e55b343185944
Thong <tho2@gmail.com>
2020-07-06 21:16:27 -0400

    Added delete and arrow functionalities

--- app/src/components/navigationbar.js
+++ app/src/components/navigationbar.js
@@ -10,50 +10,77 @@ import './css/nav.css';
 
 class Navigation extends Component{
     constructor(props){
-        super(props);
-        this.state = {
-          open: false
-        }
-
-        this.handleChange = this.handleChange.bind(this)
-        this.openPopup = this.openPopup.bind(this);
-        this.closePopup = this.closePopup.bind(this);
-
-        this.addStart = this.addStart.bind(this);
-        this.addStation = this.addStation.bind(this);
-        this.addEnd = this.addEnd.bind(this);
-
+      super(props);
+      this.state = {
+        open: false
       }
 
-      openPopup(){
-        this.setState({
-          open: true
-        });
-        console.log("Open Popup");
-      }
-      
-      closePopup(){
-        this.setState({
-          open: false
-        });
-        console.log("Close Popup");
-      }
+      this.handleChange = this.handleChange.bind(this)
+      this.openPopup = this.openPopup.bind(this);
+      this.closePopup = this.closePopup.bind(this);
+
+      this.addStart = this.addStart.bind(this);
+      this.addStation = this.addStation.bind(this);
+      this.addEnd = this.addEnd.bind(this);
+
+      this.addArrowMode = this.addArrowMode.bind(this);
+
+      this.handleRemoveMode = this.handleRemoveMode.bind(this);
+
+      this.handleReset = this.handleReset.bind(this);
+
+    }
+
+    // Open popup for adding node
+    openPopup(){
+      this.setState({
+        open: true
+      });
+      console.log("Open Popup");
+    }
+    
+    // Close popup for adding node
+    closePopup(){
+      this.setState({
+        open: false
+      });
+      console.log("Close Popup");
+    }
 
     handleChange(e){
         this.props.handleIteration(e.target.value)
     }
 
+    // add start node
     addStart(){
       this.setState({open: false});
       this.props.handleAddNode("start");
     }
+
+    // add station node
     addStation(){
       this.setState({open: false});
       this.props.handleAddNode("station");
     }
+
+    // add end node
     addEnd(){
       this.setState({open: false});
       this.props.handleAddNode("end");
+    }
+
+    // Change to add arrow mode
+    addArrowMode(){
+      this.props.addArrowMode();
+    }
+
+    // Change to remove node/arrow mode
+    handleRemoveMode(){
+      this.props.handleRemoveMode();
+    }
+
+    handleReset(){
+      this.props.handleReset();
     }
 
     render(){
@@ -67,31 +94,34 @@ class Navigation extends Component{
                 <button className="button" style={{backgroundColor:'#2cbebe'}} onClick={this.openPopup}>+</button>
               </li>
               <li class="nav-item">
-                <button className="button" style={{backgroundColor:'#2cbebe'}}>→</button>
+                <button className="button" style={{backgroundColor:'#2cbebe'}} onClick={this.addArrowMode}>→</button>
               </li>
               <li class="nav-item">
-                <button className="button" style={{backgroundColor:'red'}}>x</button>
+                <button className="button" style={{backgroundColor:'red'}} onClick={this.handleRemoveMode}>x</button>
+              </li>
+              <li class="nav-item">
+                <button className="button" style={{backgroundColor:'red'}} onClick={this.handleReset}>Reset</button>
               </li>
             </ul>
 
             <ul class="navbar-nav  " style={{float: 'right'}}>
               {/*fix format*/}
-              <li class="nav-item">
-                <label className="label" style={{color:'white'}}>Iterations: </label>
-              </li>
-              <li class="nav-item active">    
-                <input type="text" id="iteration" className="textbox" value={this.props.iteration} onChange={this.handleChange}></input>
-              </li>
-              <li class="nav-item active">
+ {//             <li class="nav-item">
+  //              <label className="label" style={{color:'white'}}>Iterations: </label>
+  //            </li>
+  //            <li class="nav-item active">    
+  //              <input type="text" id="iteration" className="textbox" value={this.props.iteration} onChange={this.handleChange}></input>
+  //            </li>
+}             <li class="nav-item active">
                 <button className="button" style={{backgroundColor:'#4CAF50'}}>Start</button>
               </li>
               <li class="nav-item active">
                 <button className="button" style={{backgroundColor:'red'}}>Stop</button>
               </li>
-              <li class="nav-item active">
-                <button className="button" style={{backgroundColor:'yellow', color:"black"}}>Data</button>
-              </li>
-              <li class="nav-item active">
+    {//          <li class="nav-item active">
+      //          <button className="button" style={{backgroundColor:'yellow', color:"black"}}>Data</button>
+    //          </li>
+    }         <li class="nav-item active">
                 <button className="button" style={{backgroundColor:'yellow', color:"black"}}>Clear</button>
               </li>
             </ul>
@@ -99,6 +129,7 @@ class Navigation extends Component{
           </div>
 
           <div>
+            {/*Popup for user to select node to add*/}
             <Popup open={this.state.open} closeOnDocumentClick = {true} onClose={this.closePopup}>
               <button onClick={this.addStart} >
                 <img src={StartImage} alt="start" />
